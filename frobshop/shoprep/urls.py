24fbab68c71e888bad1bddcc0b09ee7fe6aa84b0
harriebird <pasinero@innovuze.com>
2017-11-16 17:09:01 +0800

    improved heard and products view

--- frobshop/shoprep/urls.py
+++ frobshop/shoprep/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from oscar.core.loading import get_class
 
 app_name = 'shoprep'
-catalogue_view = get_class('catalogue.views', 'CatalogueView')
+home_view = get_class('promotions.views', 'HomeView')
 urlpatterns = [
-    url(r'^$', catalogue_view.as_view(), name='index'),
+    url(r'^$', home_view.as_view(), name='index'),
 ]
--- frobshop/shoprep/views.py
+++ frobshop/shoprep/views.py
@@ -1,11 +1,1 @@
 from django.shortcuts import render
-# from django.core.urlresolvers import reverse
-from django.views.generic import RedirectView, TemplateView
-from oscar.core.loading import get_class, get_model
-from oscar.apps.catalogue.models import AbstractProduct
-
-
-# Create your views here.
-class IndexView(TemplateView):
-    model = AbstractProduct
-    template_name = 'templates/home.html'
